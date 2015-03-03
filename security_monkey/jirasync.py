22ce3418ccc97f341a981fd36f48f7c62b81fb59
Quentin Long <qlo@yelp.com>
2015-03-03 14:27:54 -0800

    Added docs

--- security_monkey/jirasync.py
+++ security_monkey/jirasync.py
@@ -10,7 +10,6 @@
 import datetime
 import hashlib
 import yaml
-import getpass
 
 from jira.client import JIRA
 from sqlalchemy import func
@@ -26,12 +25,17 @@ class JiraSync(object):
                 data = jf.read()
                 data = yaml.load(data)
                 self.account = data['account']
-                self.password = getpass.getpass('Password for %s: ' %(self.account))
+                self.password = data['password']
                 self.project = data['project']
                 self.server = data['server']
                 self.issue_type = data['issue_type']
                 self.url = data['url']
-        except KeyError, IOError, yaml.scanner.ScannerError
+        except KeyError as e:
+            app.logger.error('JIRA sync configuration missing required field: {}'.format(e))
+        except IOError as e:
+            app.logger.error('Error opening JIRA sync configuration file: {}'.format(e))
+        except yaml.scanner.ScannerError as e:
+            app.logger.error('JIRA sync configuration file contains malformed YAML: {}'.format(e))
 
         try:
             self.client = JIRA(self.server, basic_auth=(self.account, self.password))
@@ -42,21 +46,23 @@ class JiraSync(object):
         """ Searches for existing tickets based on a hash of the constructed summary. If one exists,
         it will update the count and preserve any leading description text. If not, it will create a ticket. """
         summary = '{0} - {1} - {2}'.format(issue, technology, account)
+        # Searching by text in JIRA sucks, instead of matching the summary field, search for summary hash
         summary_hash = hashlib.sha1(summary).digest().encode('base64')[:16]
         jql = 'project={0} and text~"{1}"'.format(self.project, summary_hash)
         issues = self.client.search_issues(jql)
         
         url = "{0}/#/issues/-/{1}/{2}/-/True/{3}/1/25".format(self.url, technology, account, issue)
         description = ("This ticket was automatically created by Security Monkey. DO NOT EDIT ANYTHING BELOW THIS LINE\n"
-                      "Number of issues: {0} (Last updated: {1})\n"
+                      "Number of issues: {0}\n"
                       "Account: {2}\n"
                       "{3}\n"
-                      "[View on Security Monkey|{4}]\n".format(count, datetime.datetime.now().isoformat(), account, summary_hash, url))
+                      "[View on Security Monkey|{4}]\n"
+                      "Last updated: {1}".format(count, datetime.datetime.now().isoformat(), account, summary_hash, url))
 
         
         if len(issues):
-            # JQL returns partial matches, find the ticket with the exact summary
             for issue in issues:
+                # Make sure we found the exact ticket
                 if issue.fields.summary == summary:
                     old_desc = issue.fields.description
                     old_desc = old_desc[:old_desc.find('This ticket was automatically created by Security Monkey')]
