b5b871045edfc8f38b5bc0b4e5b1754474092e08
Quentin Long <qlo@yelp.com>
2015-07-14 17:38:40 -0700

    Added no owner tag auditor for ec2 instances

--- security_monkey/monitors.py
+++ security_monkey/monitors.py
@@ -23,6 +23,7 @@ from security_monkey.watchers.sns import SNS
 from security_monkey.auditors.sns import SNSAuditor
 from security_monkey.watchers.sqs import SQS
 from security_monkey.watchers.ec2 import EC2
+from security_monkey.auditors.ec2 import EC2Auditor
 from security_monkey.watchers.keypair import Keypair
 from security_monkey.watchers.security_group import SecurityGroup
 from security_monkey.auditors.security_group import SecurityGroupAuditor
@@ -57,7 +58,7 @@ __MONITORS = {
     SQS.index:
         Monitor(SQS.index, SQS, None),
     EC2.index:
-        Monitor(EC2.index, EC2, None),
+        Monitor(EC2.index, EC2, EC2Auditor),
     ELB.index:
         Monitor(ELB.index, ELB, ELBAuditor),
     IAMSSL.index:
