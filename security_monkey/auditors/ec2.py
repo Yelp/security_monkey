e5c53306d8441fd40cccf0e281346c127c15de76
Quentin Long <qlo@yelp.com>
2015-07-16 14:08:04 -0700

    Added owner and creator tag validation auditors

--- security_monkey/auditors/ec2.py
+++ security_monkey/auditors/ec2.py
@@ -22,23 +22,78 @@
 
 from security_monkey.auditor import Auditor
 from security_monkey.watchers.ec2 import EC2
+from security_monkey.datastore import Item, Technology
+from security_monkey import app
+
+import yaml
+
+# Valid teams as of 7/16
+BACKUP_TEAM_LIST = ['operations', 'revenue', 'dba', 'community', 'hardware', 'ad_delivery', 'biz_data', 'growth', 'pcde', 'consumer_services', 'ad_backend', 'ad_targeting', 'releng', 'bizapp', 'spam', 'seatme', 'neteng', 'platform', 'noop', 'smi', 'test', 'statmonster', 'ugc_abuse', 'dowser', 'service_infra', 'biz', 'i18n', 'distributed_systems', 'search_infra', 'mobile', 'log_infra', 'biz_infra', 'perf_metrics', 'corpeng', 'payments', 'paasta', 'webcore', 'security', 'bam', 'consumer', 'biz_engagement', 'partnerships']
 
 
 class EC2Auditor(Auditor):
     index = EC2.index
     i_am_singular = EC2.i_am_singular
     i_am_plural = EC2.i_am_plural
+    users = []
+    teams = []
+    
+    def find_valid_creators(self):
+        """
+        Finds the names of all tracked users and saves them for validating creator tags.
+        """
+        iamuser = Technology.query.filter(Technology.name == 'iamuser').first()
+        query = Item.query.filter(Item.tech_id == iamuser.id)
+        self.users = [user.name for user in query.all()]
+
+    def find_valid_owners(self):
+        """
+        Find valid values for the owner tag. This is the set of teams defined in sensu_Handler::teams in hieradata/common.yaml.
+        When Security Monkey is deployed, this file should be available at /nail/security_monkey/common.yaml.
+        """
+        try:
+            with open('/nail/security_monkey/common.yaml', 'r') as common:
+                common_data = yaml.load(common.read())
+                self.teams = common_data['sensu_handlers::teams'].keys()
+        except Exception as e:
+            # Use a backup list of teams
+            app.logger.error("Error trying to read /nail/security_monkey/common.yaml: {0}".format(e))
+            self.teams = BACKUP_TEAM_LIST
 
     def __init__(self, accounts=None, debug=False):
         super(EC2Auditor, self).__init__(accounts=accounts, debug=debug)
+        self.find_valid_owners()
+        self.find_valid_creators()
 
-    def check_tags(self, ec2item):
+    def check_for_owner_tag(self, ec2item):
         """
         alert on missing owner tag.
         """
         tag = "EC2 instance has no owner tag"
-        severity = 1
+        severity = 3
         tags = ec2item.config.get('tags', {})
         if 'owner' not in tags:
             self.add_issue(severity, tag, ec2item, notes=None)
+
+    def check_for_creator_tag(self, ec2item):
+        """
+        alert on missing creator tag.
+        """
+        tag = "EC2 instance has no creator tag"
+        severity = 3
+        tags = ec2item.config.get('tags', {})
+        if 'creator' not in tags:
+            self.add_issue(severity, tag, ec2item, notes=None)
+
+    def check_valid_owner_tag(self, ec2item):
+        """
+        alert on an owner tag that is not a valid team name, as defined by sensu_handlers::teams
+        """
+        tag = "EC2 instance has an owner tag which doesn't match a team"
+        severity = 2
+        tags = ec2item.config.get('tags', {})
+        if 'owner' in tags:
+            if tags['owner'].strip('@yelp.com') not in self.teams:
+                notes = 'Owner tag is {0}'.format(tags['owner'])
+                self.add_issue(severity, tag, ec2item, notes=notes)
 
